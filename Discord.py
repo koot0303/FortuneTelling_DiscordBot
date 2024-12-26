7a0afbcd51ea2e52cba28c5e658d6f5a5c603f4f
koot0303 <sashou.t0303@gmail.com>
2024-12-27 05:40:13 +0900

    update Discord.py

--- Discord.py
+++ Discord.py
@@ -14,11 +14,10 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
     await tree.sync() # スラッシュコマンドを同期
 
-
 @tree.command(name="fortune", description="今日の運勢を占います")
 async def fortune(ctx: discord.Interaction):
     fortunes = ["大吉", "吉", "中吉", "小吉", "末吉", "凶", "大凶"]
-    await ctx.respond(f"あなたの今日の運勢は...{random.choice(fortunes)}です！")
+    await interaction.response.send_message(f"あなたの今日の運勢は...{random.choice(fortunes)}です！")
     print("/fortune")
 
 #起動
